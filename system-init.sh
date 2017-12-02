b3fcf6304c695d60dda34f62410dcbc2123b034d
daniel1302 <daniel.1302@gmail.com>
2017-12-02 12:21:37 +0100

    Install atom intead sublime

--- system-init.sh
+++ system-init.sh
@@ -23,13 +23,8 @@ sudo pacman -S tor-browser;
 # Install Meld
 sudo pacman -S --noconfirm meld;
 
-# Install Sublime-Text Dev (https://www.sublimetext.com/docs/3/linux_repositories.html)
-curl \
-	-O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key  \
-	--add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg \
-&& echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/dev/x86_64" \
-	| sudo tee -a /etc/pacman.conf \
-&& sudo pacman -Syu --noconfirm sublime-text;
+# Install atom editor
+sudo pacman -S --noconfirm atom;
 
 # Install nginx web server
 sudo pacman -S --noconfirm \
