64b0d72cb5235b68d9aebcd930d13444dba345fd
Daniel <daniel.1302@gmail.com>
2018-11-27 00:29:46 +0100

    Update ansible script and arch installer

--- ansible/install-arch.sh
+++ ansible/install-arch.sh
@@ -4,18 +4,34 @@
     && echo "Cannot run this script as a root. Run as normal user" \
     && exit;
 
+
 # Install minimum requirements
-sudo pacman -S          \
-    git                 \
-    ansible             \
-;
+#sudo pacman -Syu --noconfirm;
+ sudo pacman-mirrors --fasttrack && sudo pacman -Syyu --noconfirm;
+
+sudo pacman -S --noconfirm      \
+    git                         \
+    ansible                     \
+    wget;
+
 
 [[ -e ~/.ansible/plugins/modules/aur ]] && rm -rf ~/.ansible/plugins/modules/aur;
 
 
-git clone https://github.com/kewlfft/ansible-aur.git ~/.ansible/plugins/modules/aur;
+#git clone https://github.com/kewlfft/ansible-aur.git ~/.ansible/plugins/modules/aur;
+[ -e ansible-aur-git.gz ] && rm ansible-aur-git.gz;
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/ansible-aur-git.tar.gz -O ansible-aur-git.gz;
+tar xvzf ansible-aur-git.gz \
+	&& cd ansible-aur-git \
+	&& makepkg -is --noconfirm \
+	&& cd ../ \
+	&& rm ansible-aur-git;
+
+
+
 
 echo "";
 echo "";
 echo "";
 echo "Now you can run your playbook";
+sudo ansible-playbook ./main.yml
